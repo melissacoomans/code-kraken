5b4d7d81f0a854637e622528f3c0d1ed6378899d
melissacoomans <melissacoomans@gmail.com>
2023-01-17 14:43:30 +0000

    renamed component codeinput

---
+++ src/components/CodeInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row class="text-center"> voer hier je code in </v-row>
+    
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'CodeInput',
+
+    data: () => ({
+      ecosystem: [],
+      importantLinks: [],
+      whatsNext: [],
+    }),
+  }
+</script>
