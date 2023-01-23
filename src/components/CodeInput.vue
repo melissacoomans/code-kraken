cdd7fc211ff8e70ad16cc13005589115aaf9ad3c
Melissa Coomans <melissa.coomans@capgemini.com>
2023-01-23 16:54:09 +0100

    added styling and removed counter on inputs

--- src/components/CodeInput.vue
+++ src/components/CodeInput.vue
@@ -1,12 +1,11 @@
 <template>
   <v-form ref="form" v-model="valid" lazy-validation class="text-center">
     <!-- <v-col cols="12" sm="6" md="3"> -->
-    <v-col cols="5" sm="5" md="2">
+    <v-col>
       <!-- maxlength werkt niet icm number, daarom gekozen voor text. TODO: nog oplossing voor alleen numbers en overspringen naar volgende input -->
       <v-text-field
         v-for="index in 5"
         :key="index"
-        :counter="1"
         :rules="codeRules"
         :maxlength="1"
         v-model="inputValues[index - 1]"
@@ -112,3 +111,13 @@ export default {
   },
 };
 </script>
+<style lang="scss">
+.v-input {
+  display: inline-block;
+  padding: 6px;
+  max-width: 5%;
+}
+.v-text-field.v-text-field--enclosed {
+  padding: 6px;
+}
+</style>
