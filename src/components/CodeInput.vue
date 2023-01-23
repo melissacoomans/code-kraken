ecd0384c2d0ab2cf72a6b32c05981760d06d47b9
Melissa Coomans <melissa.coomans@capgemini.com>
2023-01-23 11:12:03 +0100

    refactor form

--- src/components/CodeInput.vue
+++ src/components/CodeInput.vue
@@ -5,7 +5,8 @@
       <v-text-field
         :counter="1"
         :rules="codeRules"
-        v-model="firstInput"
+        v-model="input1"
+        type="number"
         pattern="[0-9]{1}"
         maxlength="1"
         max="1"
@@ -16,7 +17,8 @@
       <v-text-field
         :counter="1"
         :rules="codeRules"
-        v-model="secondInput"
+        v-model="input2"
+        type="number"
         pattern="[0-9]{1}"
         maxlength="1"
         max="1"
@@ -27,7 +29,8 @@
       <v-text-field
         :counter="1"
         :rules="codeRules"
-        v-model="thirdInput"
+        v-model="input3"
+        type="number"
         pattern="[0-9]{1}"
         maxlength="1"
         max="1"
@@ -38,7 +41,8 @@
       <v-text-field
         :counter="1"
         :rules="codeRules"
-        v-model="fourthInput"
+        v-model="input4"
+        type="number"
         pattern="[0-9]{1}"
         maxlength="1"
         max="1"
@@ -49,7 +53,8 @@
       <v-text-field
         :counter="1"
         :rules="codeRules"
-        v-model="fifthInput"
+        v-model="input5"
+        type="number"
         pattern="[0-9]{1}"
         maxlength="1"
         max="1"
@@ -64,41 +69,67 @@
     </v-btn>
 
     <v-btn color="error" class="mr-4" @click="reset"> Clear Form </v-btn>
-
-    <span v-if="!checkCodes && validateText">{{ validateText }}</span>
+    <br />
+    <br />
+    <span v-if="validatedCodeText">{{ validatedCodeText }}</span>
+    <br />
+    <br />
     <span v-if="feedbackText">{{ feedbackText }}</span>
+    <br />
+    <br />
+    <span>winnende code: {{ winningCodesCombined }}</span>
+    <br />
+    <br />
+    <span>eerdere submitted codes: </span>
+    <span v-for="code in submittedCodes" :key="code">{{ code }}</span>
+
+    <!-- <span v-for="number in winningCode" :key="number">{{ number }}</span> -->
   </v-form>
 </template>
 
 <script>
 export default {
   name: 'CodeInput',
+  created() {
+    this.combineWinningCode();
+  },
 
   data: () => ({
     valid: true,
-    winningCode: '12345',
-    feedbackText: '',
-    name: '',
-    validateText: '',
+    winningCodesCombined: '',
+    submittedCodes: ['99999'],
+    winningCode: ['1', '2', '3', '4', '5'],
+    feedbackText: null,
+    name: null,
+    validateText: null,
     codeRules: [(v) => !!v || 'code is required'],
-    firstInput: '',
-    secondInput: '',
-    thirdInput: '',
-    fourthInput: '',
-    fifthInput: '',
-    combinedInputCodes: '',
+    inputCodesCombined: '',
+    validatedCodeText: '',
+    input1: '',
+    input2: '',
+    input3: '',
+    input4: '',
+    input5: '',
   }),
   methods: {
     combineInputCodes: function () {
-      this.combinedInputCodes = this.firstInput.concat(
-        this.secondInput,
-        this.thirdInput,
-        this.fourthInput,
-        this.fifthInput
+      this.inputCodesCombined = this.input1.concat(
+        this.input2,
+        this.input3,
+        this.input4,
+        this.input5
+      );
+    },
+    combineWinningCode: function () {
+      this.winningCodesCombined = this.winningCode[0].concat(
+        this.winningCode[1],
+        this.winningCode[2],
+        this.winningCode[3],
+        this.winningCode[4]
       );
     },
     checkCodes: function () {
-      if (this.combinedInputCodes === this.winningCode) {
+      if (this.inputCodesCombined === this.winningCodesCombined) {
         return (this.feedbackText = 'yay you are a winner');
       } else {
         return (this.feedbackText = 'booo try again!');
@@ -113,7 +144,7 @@ export default {
       this.combineInputCodes();
       this.checkCodes();
       return (this.validatedCodeText =
-        `gevalideerd! met cijfers: ` + this.combinedInputCodes);
+        `gevalideerd! met cijfers: ` + this.inputCodesCombined);
     },
     reset() {
       this.clearForm();
